6cc2537d1b935aff718d7afa0ef0e714d7f0120b
Saksham1411 <sakshamsamgarg1411@gmail.com>
2023-11-02 11:35:18 +0530

    day1-taskManger

--- Task Manger/app.js
+++ Task Manger/app.js
@@ -1,1 +1,27 @@
-console.log('Task Manager App')
+const express = require('express');
+
+const app = express();
+
+const tasks = require('./routes/tasks');
+
+// middleware
+
+app.use(express.json());
+
+
+// routes
+app.get('/hello',(req,res)=>{
+    res.send('Working!!!');
+})
+
+
+app.use('/api/v1/tasks',tasks)
+
+const port = 3000;
+
+app.listen(port, console.log('server is running...'))
+
+
+
+// 24.00
+// last work made a function in controllers tasks getAllTasks
---
+++ Task Manger/controllers/tasks.js
@@ -0,0 +1,5 @@
+const getAllTasks = ((req,res)=>{
+    res.send('workingg')
+})
+
+module.exports = {getAllTasks};
