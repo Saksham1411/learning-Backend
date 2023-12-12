3d6fd4d1bf4581315d7f547e04ca8dce8727de7e
Saksham1411 <sakshamsamgarg1411@gmail.com>
2023-12-12 19:58:48 +0530

    error handle

--- classroom/new project/backend/index.js
+++ classroom/new project/backend/index.js
@@ -8,12 +8,16 @@ app.use(express.json());
 
 app.get('/notes', async (req, res) => {
     const notes = await Note.find();
-    res.send(notes);
+    res.status(201).send(notes);
 })
 app.post('/notes', async (req, res) => {
     const { content } = req.body;
-    const notes = await Note.create({ content });
-    res.send(notes);
+    try {
+        const notes = await Note.create({ content });
+        res.send(notes);
+    } catch (error) {
+        res.status(400).send("could not create the note");
+    }
 })
 
 mongoose.connect('mongodb://127.0.0.1:27017/notes');
